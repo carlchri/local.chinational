1c05ec29b847351282fd095cb2ae2f29ef44d767
CXU\prokarma <cxu@prokarma.com>
2018-02-26 20:22:37 -0600

    Added background color

--- core/src/main/java/org/chi/aem/common/components/model/FullBleedTiles.java
+++ core/src/main/java/org/chi/aem/common/components/model/FullBleedTiles.java
@@ -5,11 +5,14 @@
 package org.chi.aem.common.components.model;
 
 import com.adobe.cq.sightly.WCMUsePojo;
+import org.apache.sling.api.resource.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FullBleedTiles extends WCMUsePojo {
 
@@ -17,19 +20,22 @@ public class FullBleedTiles extends WCMUsePojo {
 
     public static final String NO_OF_TILES = "noOfBleedTiles";
 
-    private List<String> tilesList;
+    private Map<String, String> tileMap;
     private int noOfTiles;
     private int cssClassNo;
     private String cssClass;
     private boolean isLastCss = false;
     private String lastCssClass = "";
-    
+
     @Override
     public void activate() throws Exception {
     	noOfTiles = Integer.parseInt(getProperties().get(NO_OF_TILES, "1"));
-        tilesList = new ArrayList<String>();
+        tileMap = new HashMap<String, String>();
+        Resource currentResource = getResource();
+        Resource tileResource = null;
         for (int i = 1; i <= noOfTiles; i++) {
-            tilesList.add("bleed-box" + i);
+            tileResource = currentResource.getChild("bleed-box" + i);
+            tileMap.put("bleed-box" + i,getBackGroundColor(tileResource));
 	  	}
         cssClassNo = 12/noOfTiles;
         cssClass = "col-sm-" + (cssClassNo * 2) + " " + "col-md-" + cssClassNo;
@@ -39,9 +45,16 @@ public class FullBleedTiles extends WCMUsePojo {
         }
     }
 
-    public List<String> getTilesList() {
+    private String getBackGroundColor(Resource tileResource) {
+        if(tileResource != null) {
+            return tileResource.getValueMap().get("backgroundColor", null);
+        }
+        return null;
+    }
+
+    public Map<String, String> getTileMap() {
  	      //LOGGER.info("getTilesList Called :" + tilesList);
-    	  return tilesList;
+    	  return tileMap;
     }
     
     public String getCssClass() {
