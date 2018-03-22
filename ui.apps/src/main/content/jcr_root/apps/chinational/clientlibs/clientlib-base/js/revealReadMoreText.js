2989706e1fc2bfa4b466aa3d05787474bd1008e1
ndevnani <ndevnani@prokarma.com>
2018-03-22 12:57:26 -0600

    spacing fix

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/revealReadMoreText.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/revealReadMoreText.js
@@ -11,7 +11,7 @@ $(".sidebar-box .button").click(function() {
   
   // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
   $ps.each(function() {
-    totalHeight += $(this).outerHeight()+7;
+    totalHeight += $(this).outerHeight()+12;
   });
         
   $up
