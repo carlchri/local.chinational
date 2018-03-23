b8c6365f2654a7c691b8f98e25549c5980e3ba80
dhooda <dhooda@prokarma.com>
2018-03-23 16:22:27 -0400

    newslist pagination and Ajax

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/newslist.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/newslist.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
 
+    var start_index = 0;
+    var NEWS_HITS_PER_PAGE = 10;
+
+    var total_results = parseInt($('#total_results').val());
+
+	newsLoadMoreShowHide();          
+
     $('#search_news_list').on('change', function() {
       if (this.value == 'By Year')
       {
@@ -10,28 +17,73 @@ $(document).ready(function(){
       else
       {
         $("#select_news_by_year").hide();
-         $("#filter_news_form").submit();
+        $("#select_news_by_year option:selected").removeAttr("selected");
+        $("#select_news_by_year option[value='Choose Year']").attr('selected', 'selected');  
+        start_index = 0;
+
+        // to clear contents on the page  
+        $('.filtered_list').html("");
+
+        newslistAjaxCall();
       }
     });
 
     $('#search_news_year').on('change', function() {
-        $("#filter_news_form").submit(); 
+          if (this.value != 'Choose Year')
+          {
+            start_index = 0;
+            $('.filtered_list').html("");
+            newslistAjaxCall();
+          }
     });
-
-    var size_li = $(".filtered_list li").size();
-    var x=10;
-    $('.filtered_list li:lt('+x+')').show();
-    
-    if(x >= size_li) {
-	    $('.filtered_list_show_more').hide();
-    }
 
     $('.filtered_list_show_more').click(function () {
-        x= (x+10 <= size_li) ? x+10 : size_li;
-        $('.filtered_list li:lt('+x+')').show();
-        if(x >= size_li) {
-			    $('.filtered_list_show_more').hide();
-        }
+		start_index += NEWS_HITS_PER_PAGE; 
+        newslistAjaxCall();
     });
 
+    function newsLoadMoreShowHide() {
+        // Compare size of results with total results and hide LOAD MORE, if not required
+        size_li = $(".filtered_list li").size();
+        $('.filtered_list_show_more').show();        
+        if(size_li >= total_results) {
+	        $('.filtered_list_show_more').hide();
+        }
+	}
+
+    function newslistAjaxCall() {
+        //Get the user-defined values
+        var search_news_list = $('#search_news_list').val() ; 
+        var search_news_year = $('#search_news_year').val() ; 
+        var media_page_path = $('#media_page_path').val() ; 
+		var current_page_path = $('#current_page_path').val() ; 
+
+   		var servletURL = current_page_path + '.newsservlet.' + search_news_list + '.' + search_news_year + '.' + start_index + '.html';
+
+        $.ajax({
+            type: 'GET',    
+            url: servletURL,
+            // data:'search_news_list='+ search_news_list+'&search_news_year='+ search_news_year+'&media_page_path='+ media_page_path+'&start_index='+ start_index,
+            success: function(msg){
+                var json = jQuery.parseJSON(msg); 
+                total_results = json.total_results;            
+                $.each(json.jsonNews, function(index, item) {
+            		$("<li>").append(
+            			$("<article>").append(
+						$("<a href=" + item.newsURL + ".html>").append(
+            				$("<h3>").append(
+            					$("<span class='txt-green'>").text(item.newsHeading)
+            				)
+            			),
+                        $("<span class='txt-green'>").text(item.publishDate),
+                        $("<p>").text(item.excerpt)
+             		)).appendTo(".filtered_list");
+                });
+				newsLoadMoreShowHide();          
+            },
+			error: function (err) {
+	           	alert(err);
+            }                
+        });
+    }
 });
