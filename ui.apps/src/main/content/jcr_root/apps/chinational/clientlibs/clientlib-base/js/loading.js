3eb01016c4e81eac35fe99051191396ce5e0e28a
dhooda <dhooda@prokarma.com>
2018-03-30 17:03:31 -0400

    html cahnges

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/loading.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/loading.js
@@ -1,6 +1,7 @@
+// DO NOT REQUIRE THIS FILE - CAN BE DELETED IN AEM
 $(document).ready(function(){
 	// console.log('Loading Page');
-	$('.loading').fadeOut('slow');
+	// $('.loading').fadeOut('slow');
 
 	$("#showButtonLoad").on('click',function(){
 		$('.loading').fadeIn('slow');
