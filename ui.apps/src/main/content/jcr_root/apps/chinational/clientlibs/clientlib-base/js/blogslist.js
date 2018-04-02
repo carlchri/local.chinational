59880a61314834be05ab343dfac47380d86034ea
dhooda <dhooda@prokarma.com>
2018-04-02 18:48:08 -0400

    bloglist changes

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/blogslist.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/blogslist.js
@@ -1,37 +1,92 @@
 
 $(document).ready(function(){
 
+    var blogs_start_index = 0;
+    var blogs_filter = "SortByMostRecent";
+    var BLOGS_HITS_PER_PAGE = 10;
+
+    var blogs_total_results = parseInt($('#total_results').val());
+
+	blogsLoadMoreShowHide();          
+
     $('#search_blogs_list').on('change', function() {
-      if (this.value == 'By Year')
+      if (this.value == 'ByYear')
       {
         $("#select_blogs_by_year").show();
-
       }
       else
       {
         $("#select_blogs_by_year").hide();
-         $("#filter_blogs_form").submit();
+        $("#select_blogs_by_year option:selected").removeAttr("selected");
+        $("#select_blogs_by_year option[value='ChooseYear']").attr('selected', 'selected');  
+        blogs_start_index = 0;
+
+        // to clear contents on the page  
+        $('.filtered_blogs_list').html("");
+
+        blogs_filter = $('#search_blogs_list').val() ;
+		$('.loading_blogs').show();
+        blogslistAjaxCall();
       }
     });
 
     $('#search_blogs_year').on('change', function() {
-        $("#filter_blogs_form").submit(); 
+          if (this.value != 'ChooseYear')
+          {
+            blogs_start_index = 0;
+            $('.filtered_blogs_list').html("");
+            blogs_filter = $('#search_blogs_year').val() ;            
+    		$('.loading_blogs').show();
+            blogslistAjaxCall();
+          }
     });
-
-    var size_li = $(".filtered_blogs_list li").size();
-    var x=10;
-    $('.filtered_blogs_list li:lt('+x+')').show();
-    
-    if(x >= size_li) {
-	    $('.filtered_blogs_list_show_more').hide();
-    }
 
     $('.filtered_blogs_list_show_more').click(function () {
-        x= (x+10 <= size_li) ? x+10 : size_li;
-        $('.filtered_blogs_list li:lt('+x+')').show();
-        if(x >= size_li) {
-			    $('.filtered_blogs_list_show_more').hide();
-        }
+		blogs_start_index += BLOGS_HITS_PER_PAGE; 
+		$('.loading_blogs_next').show();
+        blogslistAjaxCall();
     });
 
+    function blogsLoadMoreShowHide() {
+        // Compare size of results with total results and hide LOAD MORE, if not required
+        size_li = $(".filtered_blogs_list li").size();
+        $('.filtered_blogs_list_show_more').show();        
+        if(size_li >= blogs_total_results) {
+	        $('.filtered_blogs_list_show_more').hide();
+        }
+	}
+
+    function blogslistAjaxCall() {
+        $('.filtered_blogs_list_show_more').hide();
+		var current_page_path = $('#current_page_path').val() ; 
+
+   		var servletURL = current_page_path + '.blogsservlet.' + blogs_filter + '.' + blogs_start_index + '.json';
+
+        $.ajax({
+            type: 'GET',    
+            url: servletURL,
+            success: function(msg){
+                var json = jQuery.parseJSON(msg); 
+                blogs_total_results = json.total_results;            
+        		$('.loading_blogs').hide();    
+        		$('.loading_blogs_next').hide();    
+                $.each(json.jsonBlogs, function(index, item) {
+            		$("<li>").append(
+            			$("<article>").append(
+						$("<a href=" + item.blogsURL + ".html>").append(
+            				$("<h3>").append(
+            					$("<span class='txt-green blogs_heading_hover'>").text(item.blogsHeading)
+            				)
+            			),
+                        $("<span class='txt-green'>").text(item.publishDate),
+                        $("<p>").text(item.excerpt)
+             		)).appendTo(".filtered_blogs_list");
+                });
+				blogsLoadMoreShowHide();          
+            },
+			error: function (err) {
+	           	alert("Error in Loading.");
+            }                
+        });
+    }
 });
