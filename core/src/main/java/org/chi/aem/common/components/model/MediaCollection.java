b73fb0357aa2f13076827e5d42b6605648a175c8
CXU\prokarma <cxu@prokarma.com>
2018-04-09 18:30:53 -0500

    Save Carousel item node name to mediacarousel node. When content author update carousel item properties, the node name stay the same(don't hash again).

--- core/src/main/java/org/chi/aem/common/components/model/MediaCollection.java
+++ core/src/main/java/org/chi/aem/common/components/model/MediaCollection.java
@@ -6,6 +6,8 @@ import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceUtil;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.jcr.Node;
@@ -13,17 +15,16 @@ import javax.jcr.Session;
 import java.util.*;
 
 public class MediaCollection extends WCMUsePojo {
+	public static final Logger LOGGER = LoggerFactory.getLogger(MediaCollection.class);
 	private List<CarouselItem> editViewItems;
 	private List<List<CarouselItem>> previewItems;
 	private CarouselItem firstItem;
 	private String sectionHeading;
 	static final String MEDIA_COLLECTION = "mediaCollection";
+	static final String NODE_NAME = "nodeName";
 
 	@Override
 	public void activate() throws Exception {
-		Resource mediaCarouselR = getResource();
-		ValueMap properties = ResourceUtil.getValueMap(mediaCarouselR);
-		sectionHeading = properties.get("sectionHeading", "");
 		setEditViewItems();
 	}
 
@@ -42,22 +43,31 @@ public class MediaCollection extends WCMUsePojo {
 		Resource mediaCollectionR = getResource().getChild(MEDIA_COLLECTION);
 		CarouselItem item = null;
 		Session session = getResourceResolver().adaptTo(Session.class);
+		Resource mediaCarouselR = getResource();
+		ValueMap carouselProps = ResourceUtil.getValueMap(mediaCarouselR);
+		sectionHeading = carouselProps.get("sectionHeading", "");
 		if(mediaCollectionR != null) {
+			Node carouselNode = mediaCarouselR.adaptTo(Node.class);
 			Iterable<Resource> medias = mediaCollectionR.getChildren();
 			for(Resource aMedia : medias) {
 				ValueMap properties = ResourceUtil.getValueMap(aMedia);
 				int nodeName = getHash(aMedia.getPath());
 				if(properties.containsKey("name")) {
-					if(!properties.containsKey("nodeName")) {
-						try {
-							Node itemNode = aMedia.adaptTo(Node.class);
-							itemNode.setProperty("nodeName", nodeName);
+					try {
+						Node itemNode = aMedia.adaptTo(Node.class);
+						if(!properties.containsKey(NODE_NAME) && !carouselProps.containsKey(itemNode.getName())) {
+								itemNode.setProperty(NODE_NAME, nodeName);
+							carouselNode.setProperty(itemNode.getName(), nodeName);
+								session.save();
+						}else if(carouselProps.containsKey(itemNode.getName())){
+							nodeName = ((Long) carouselProps.get(itemNode.getName())).intValue();
+						}else {
+							nodeName = ((Long) properties.get(NODE_NAME)).intValue();
+							carouselNode.setProperty(itemNode.getName(), nodeName);
 							session.save();
-						}catch (Exception e) {
-
 						}
-					}else {
-						nodeName = ((Long) properties.get("nodeName")).intValue();
+					}catch (Exception e) {
+						LOGGER.error("Error - MediaCollection.setEditViewItems: " + e.getMessage());
 					}
 					item = new CarouselItem(nodeName,properties.get("name", String.class), properties.get("mediaType", String.class));
 					editViewItems.add(item);
