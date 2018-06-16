aaa8fc63e692045663d0b66290b151e74afac7f2
dhooda <dhooda@prokarma.com>
2018-06-16 00:52:34 -0400

    CHIP28 photoBandWithForm changes, placeholder template, calloutTiles placeholder template, campaign landing template changes

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/photoBandWithForm.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/photoBandWithForm.js
@@ -10,4 +10,27 @@ $(document).ready(function () {
             }
         });
     });
+    
+    if ($( "body" ).hasClass( "campaignlandingpage" )) {
+    	var $headerHeight = $('.campaignlandingpage .header');
+    	// alert($headerHeight.outerHeight(true));
+    	$('.campaignlandingpage .body-content').css( "margin-top", $headerHeight.outerHeight(true)+'px' );
+    	// $(".campaignlandingpage .body-content").css( "margin-top", "112px" );
+        }
+    
+    textScaleToSize();
+    
+    function textScaleToSize() {
+    	  var elements = $('.photo-band-with-form .intro-caption-white');
+    	  if(elements.length < 0) {
+    	    return;
+    	  }
+    	  elements.each(function(i, element) {
+    	    while(element.scrollWidth > element.offsetWidth || element.scrollHeight > element.offsetHeight) {
+    	      var newFontSize = (parseFloat($(element).css('font-size').slice(0, -2)) * 0.95) + 'px';
+    	      $(element).css('font-size', newFontSize);
+    	    }
+    	  });
+    	}
+
 });
