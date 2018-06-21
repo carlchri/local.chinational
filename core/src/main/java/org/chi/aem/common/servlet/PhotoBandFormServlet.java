e76d2e90003a06eb73dc434549d4a8599fc01d7d
dhooda <dhooda@prokarma.com>
2018-06-20 22:35:32 -0400

    photoBandForm Component CHIP28 changes

--- core/src/main/java/org/chi/aem/common/servlet/PhotoBandFormServlet.java
+++ core/src/main/java/org/chi/aem/common/servlet/PhotoBandFormServlet.java
@@ -5,8 +5,10 @@ import com.adobe.acs.commons.email.EmailService;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.rmi.ServerException;
-  
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
  
@@ -66,47 +68,86 @@ public class PhotoBandFormServlet extends SlingAllMethodsServlet {
     private static String TEMPLATE_PATH = "/etc/notification/email/chinational/emailTemplate.html";
     private static String SENDER_EMAIL = "admin@adobe.com";
     private static String SUBJECT = "Campaign Form Submitted";
-    private static String RECIPIENT_NAME = "Naresh Devnani";
     private static String CC_EMAIL = "CC: davinderhooda@gmail.com\nCC: maildhooda@gmail.com";
-    private static String BCC_EMAIL = "BCC: davinder_hooda@yahoo.co.in";
-    private static String EMAIL_BODY = "hello there";
-    private static String SENDER_NAME = "Davinder Hooda";
+    // private static String EMAIL_BODY = "Here are the details of the Campaign form submitted";
+    // private static String SENDER_NAME = "CHI Web Team"; 
+    // private static String RECIPIENT_NAME = "CHI Campaign Team";
+    private static String[] TO_EMAIL = { "dhooda@prokarma.com" };
     
-    private static String SUCCESS_MESSAGE = "Email has been sent.";
-    private static String FAILURE_MESSAGE = "Can not sent out email.";
-    private static String INVALID_EMAIL_ADDRESS = "Invalid Email Address.";
+    //Set the dynamic vaiables of your email template
+	private Map<String, String> emailParams;
+	// Specify the template file to use (this is an absolute path in the JCR)
+	private String templatePath;
+	// Array of email recipients. For testing, you will want to change this to your email address so you can recieve the email
+	private String[] recipients;
 
-             
-    @Override
+	private Map<String, String[]> formParams;
+	
+	@Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServerException, IOException {
     	
-		LOGGER.info("Inside emailutils activate method");
-    	// Specify the template file to use (this is an absolute path in the JCR)
-    	String templatePath = TEMPLATE_PATH;
-    	//Set the dynamic vaiables of your email template
-    	Map<String, String> emailParams = new HashMap<String,String>();
-    	emailParams.put("message", EMAIL_BODY);
+    	// LOGGER.info("Inside photoband form servlet doPost method");
+    	emailParams = new HashMap<String,String>();
+    	formParams = new HashMap<String,String[]>();
+    	templatePath = TEMPLATE_PATH;
+    	
+    	//populate Email parameters with default values
+    	// emailParams.put("message", EMAIL_BODY);
     	//  Customize the sender email address - if required
     	emailParams.put("senderEmailAddress", SENDER_EMAIL);
-    	emailParams.put("senderName", SENDER_NAME);
-    	emailParams.put("recipientName", RECIPIENT_NAME);
+    	// emailParams.put("senderName", SENDER_NAME);
     	emailParams.put("ccEmail", CC_EMAIL);
-    	emailParams.put("bccEmail", BCC_EMAIL);
     	emailParams.put("subject", SUBJECT);
-
-    	// Array of email recipients
-    	String[] recipients = { "dhooda@prokarma.com" }; //you will want to change this to your email address so you can recieve the email
-    	// emailService.sendEmail(..) returns a list of all the recipients that could not be sent the email
-    	// An empty list indicates 100% success
-/*        try {
-        	emailService = emailServiceWrapper.getEmailServiceWrapper();
-        }
-        catch(Exception e)
-        {
-         LOGGER.info("Exception to Email Service");
-         e.printStackTrace();
-        }
-*/
+    	// emailParams.put("recipientName", RECIPIENT_NAME);
+    	recipients = TO_EMAIL;
+    	
+    	// populate Email parameters with values from dialog configured by author
+        Resource resource = request.getResource();
+        LOGGER.info("resource path : " + resource.getPath());
+		if (resource != null) {
+			Iterator<Resource> childResources = resource.listChildren();
+			while (childResources.hasNext()) {
+				 Resource emailResource = childResources.next().getChild("main-respgrid/photobandwithform");
+				    if (emailResource == null) {
+				        continue;
+				    }
+				    if (emailResource != null) {
+				    	LOGGER.info("emailResource path : " + emailResource.getPath());
+				    	populateEmailParams(emailResource);
+				    	break;
+				    }
+			}
+		}
+		
+		// get form field name and values for emailTemplate
+		formParams = request.getParameterMap();
+    	LOGGER.info("formParams Size : " + formParams.size());
+    	int i=1;
+    	for(String key: formParams.keySet()){
+    		if(!key.equals("current_page_path")){
+    			emailParams.put("fieldName"+i, key + ": ");
+    			String[] fieldValueArray = formParams.get(key);
+    			String fieldValue = "";
+    			for(int j=0; j<fieldValueArray.length; j++){
+    					fieldValue += formParams.get(key)[j] + ", ";
+    			}
+    			if(fieldValue.trim().endsWith(",")){
+    				LOGGER.info("inside form trim - fieldValue : " + fieldValue);
+    				fieldValue = fieldValue.substring(0, fieldValue.trim().lastIndexOf(","));
+    			}
+    			emailParams.put("fieldName"+i+"Value", fieldValue);
+    		}
+    		LOGGER.info("fieldName"+i + " : " + key);
+    		LOGGER.info("fieldName"+i + "Value : " + emailParams.get("fieldName"+i+"Value"));
+    		i++;
+    	}
+    	
+    	// If form fields are less than 4, to clear extra field values from emailTemplate 
+    	for(int k=formParams.size(); k < 5; k++){
+    		emailParams.put("fieldName"+k, "");
+    		emailParams.put("fieldName"+k+"Value", "");
+    	}
+    
     	// emailService = getSlingScriptHelper().getService(EmailService.class);
     	LOGGER.info("emailService : " + emailService);
     	List<String> failureList = emailService.sendEmail(templatePath, emailParams, recipients);
@@ -117,13 +158,13 @@ public class PhotoBandFormServlet extends SlingAllMethodsServlet {
     		LOGGER.info("Email sent failed");
     	}
     	LOGGER.info("failureList" + failureList);
-    	LOGGER.info("request.getParameterMap() : " + request.getParameterMap());
+    	LOGGER.info("request.getParameterMap() : " + request.getParameterMap().size());
     	LOGGER.info("request.getParameterNames() : " + request.getParameterNames());
     		
 	        // response.setContentType("application/json");
 	         
 	       PrintWriter out = response.getWriter();
-	       out.write("Hello");
+	       out.write("Form Submitted");
 	       out.flush();
     }    
     
@@ -132,5 +173,47 @@ public class PhotoBandFormServlet extends SlingAllMethodsServlet {
             throws ServletException, IOException {
         doPost(request, response);
     }
+    
+    public void populateEmailParams(Resource resource) {
+    	LOGGER.info("inside populateEmailParams()");
+    	
+        try {
+	            ValueMap sMap = resource.getValueMap();
+	            if(sMap.get("emailFrom", String.class) != null) {
+	            	emailParams.put("senderEmailAddress", sMap.get("emailFrom", String.class));
+	            }
+	            if(sMap.get("emailTo", String.class) != null) {
+	            	recipients[0] = sMap.get("emailTo", String.class);
+	            	LOGGER.info("emailTo : " + recipients[0]);
+	            }
+	            if(sMap.get("emailSubject", String.class) != null) {
+	            	emailParams.put("subject", sMap.get("emailSubject", String.class));
+	            }
+	            
+	            String ccEmail="CC: ";
+	            Resource ccResource = resource.getChild("ccEmails");
+	    		if (ccResource != null) {
+	    			Iterator<Resource> ccResources = ccResource.listChildren();
+	    			int i =1;
+	    			while (ccResources.hasNext()) {
+	    				String property = ccResources.next().getValueMap().get("ccEmail", String.class);
+	    				ccEmail += property + "\nCC: ";
+	    			}
+	    		}
+	    		if(ccEmail.trim().endsWith("\nCC:")){
+	    			LOGGER.info("inside ccEmail trim");
+	    			ccEmail = ccEmail.substring(0, ccEmail.trim().lastIndexOf("\nCC:"));
+	    		}
+	            LOGGER.info("ccEmail : " + ccEmail);
+	        	emailParams.put("ccEmail", ccEmail);
+	        
+        } catch (Exception e) {
+        	LOGGER.info("Exception occured in populateEmailParams()");
+            LOGGER.error("Exception Occured in populateEmailParams() method" + e, e);
+        }
+
+    }
+
+    
 
 }
--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/photoBandWithForm.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/photoBandWithForm.js
@@ -1,16 +1,5 @@
 $(document).ready(function () {
 
-/*    $("#photo-band-form-select").change(function () {
-        $("option", $(this)).each(function (index) {
-            if ($(this).is(":selected")) {
-                $(this).css("backgroundColor", "#FFFFFF");
-            }
-            else {
-                $(this).css("backgroundColor", "#ddf4f8");
-            }
-        });
-    });
-*/    
     if ($( "body" ).hasClass( "campaignlandingpage" )) {
     	var $headerHeight = $('.campaignlandingpage .header');
     	// alert($headerHeight.outerHeight(true));
@@ -18,68 +7,228 @@ $(document).ready(function () {
     	// $(".campaignlandingpage .body-content").css( "margin-top", "112px" );
         }
     
-    function textScaleToSize() {
-    	  var elements = $('.photo-band-with-form .intro-caption-white');
-    	  if(elements.length < 0) {
-    	    return;
-    	  }
-    	  elements.each(function(i, element) {
-    	    while(element.scrollWidth > element.offsetWidth || element.scrollHeight > element.offsetHeight) {
-    	      var newFontSize = (parseInt($(element).css('font-size').slice(0, -2)) - 1) + 'px';
-    	      $(element).css('font-size', newFontSize);
-    	    }
-    	  });
-    	}
-    
-    $('#photo-band-form-select').multiselect({
-  	  nonSelectedText: 'Dropdown list',
-  	  enableFiltering: false,
-  	  enableCaseInsensitiveFiltering: false
-  	  // buttonWidth:'380px'
-  	 });
+    // $.each($('.photo-band-form-select'), function (index, value) {
+    $('.photo-band-form-select').each(function (index) { 
+    	var selectName = $(this).attr('name');
+    	$(this).multiselect({
+    	  nonSelectedText: selectName,
+	  	  enableFiltering: false,
+	  	  enableCaseInsensitiveFiltering: false
+	  	  // buttonWidth:'380px'
+	  	 });
+    });
 
-    // $('#photo-band-form-multiselect').multipleSelect();
-      $('#photo-band-form-multiselect').multiselect({
-    	  nonSelectedText: 'Checkbox list',
+    // $('.photo-band-form-multiselect').multipleSelect();
+    // $.each($('.photo-band-form-multiselect'), function (index, value) {
+    $('.photo-band-form-multiselect').each(function (index) { 
+      var selectName = $(this).attr('name');
+      $(this).multiselect({
+    	  nonSelectedText: selectName,
     	  enableFiltering: false,
     	  enableCaseInsensitiveFiltering: false
     	  // buttonWidth:'380px'
     	 });
-      
-      $('.photo-band-form').on('submit', function(event){
+    });
+
+    function validateForm(){
+
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        var numberReg = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
+        
+		$('.photo-band-form').find('input').each(function(){
+			// alert("found input field in form");
+			$(this).css('border', 'inherit');
+		    if($(this).prop('required')){
+				// alert("required field");
+				if ($(this).val() === "") {
+					//alert("inside empty validate");
+					  $(this).css('border', 'solid 2px red');
+			          $("#pbf_error_message").show();
+			          $(this).focus();
+			          return false;
+			        }else if($(this).attr("type") == "email" && !emailReg.test($(this).val())){
+			        	// alert("inside email validate");
+					  $(this).css('border', 'solid 2px red');
+			          $("#pbf_error_message").text("Please provide valid e-mail address.");
+			          $("#pbf_error_message").show();
+			          $(this).focus();
+			          return false;
+			        } else if($(this).attr("type") == "tel" && !numberReg.test($(this).val())){
+			        	//alert("inside tel validate");
+						  $(this).css('border', 'solid 2px red');
+				          $("#pbf_error_message").text("Please provide valid telephone address.");
+				          $("#pbf_error_message").show();
+				          $(this).focus();
+				          return false;
+				        }
+		    }
+		    
+			$('.photo-band-form').find('.photo-band-form-multiselect, .photo-band-form-select').each(function(){
+				// alert("found select field in form");
+				$(this).siblings('.btn-group').css('border', 'none');
+			    if($(this).prop('required')){
+			    	var selectIsEmpty = true;
+					// alert("required field");
+			    	var form_data = $('.photo-band-form').serializeArray();
+			    	// alert(form_data[2].name);
+			    	var $select_name = $(this).attr('name');
+			    	// alert($select_name)
+					$.each(form_data, function(i, field){
+						if(field.name == $select_name){
+							selectIsEmpty = false;
+						}
+					});
+
+					if (selectIsEmpty) {
+						  $(this).siblings('.btn-group').css('border', 'solid 2px red');
+				          $("#pbf_error_message").show();
+				          $(this).siblings('.btn-group .button').focus();
+				          return false;
+				        }
+			    }
+			});    		
+		    
+		});    		
+    }   
+    
+    //('.photo-band-form').on('submit', function(event){
+    $("#photo-band-form-button").click(function() {			
   	      // alert("inside submit");
     	  event.preventDefault();
-    	  var form_data = $(this).serialize();
-    	  // alert(form_data);
+    	  var submitForm=false;
+          $("#pbf_error_message").text("The above highlighted field is required.");
           $('#pbf_success_message').hide();
-          var current_page_path = $('#current_page_path').val() ; 
-
-          var servletURL = current_page_path + '.photoBandFormServlet.html';
+          $('#pbf_error_message').hide();
+          var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+          var numberReg = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})$/;
+          var total_validation_items = $('.photo-band-form').find('input, .photo-band-form-multiselect, .photo-band-form-select').length;
+  		$('.photo-band-form').find('input, .photo-band-form-multiselect, .photo-band-form-select').each(function(ind){
+  			if($(this).is("input")){
+  				// alert("inside input");
+	  			$(this).css('border', 'inherit');
+	  		    if($(this).prop('required')){
+	  				// alert("required field");
+	  				if ($(this).val() === "") {
+	  					//alert("inside empty validate");
+	  					  $(this).css('border', 'solid 2px red');
+	  			          $("#pbf_error_message").show();
+	  			          $(this).focus();
+	  			          return false;
+	  			        }else if($(this).attr("type") == "email" && !emailReg.test($(this).val())){
+	  			        	alert("inside email validate");
+	  					  $(this).css('border', 'solid 2px red');
+	  			          $("#pbf_error_message").text("Please provide valid e-mail address.");
+	  			          $("#pbf_error_message").show();
+	  			          $(this).focus();
+	  			          return false;
+	  			        } else if($(this).attr("type") == "tel" && !numberReg.test($(this).val())){
+	  			        	//alert("inside tel validate");
+	  						  $(this).css('border', 'solid 2px red');
+	  				          $("#pbf_error_message").text("Please provide valid telephone address.");
+	  				          $("#pbf_error_message").show();
+	  				          $(this).focus();
+	  				          return false;
+	  				        }
+	  		    }
+  			}
+  			if($(this).is("select")){
+  				// alert("inside select");
+  				$(this).siblings('.btn-group').css('border', 'none');
+  			    if($(this).prop('required')){
+  			     	window.selectIndex = ind;
+  			     	//alert(window.selectIndex);
+  			    	window["selectIsEmpty"+window.selectIndex] = true;
+  			    	
+  			    	// selectIsEmpty['select'+i] = true;
+  					//alert("required field");
+  			    	var form_data = $('.photo-band-form').serializeArray();
+  			    	// alert(form_data[2].name);
+  			    	var $select_name = $(this).attr('name');
+  			    	// alert($select_name)
+  					$.each(form_data, function(i, field){
+  						if(field.name == $select_name){
+  							window["selectIsEmpty"+window.selectIndex] = false;
+  						}
+  					});
+  					//alert(window["selectIsEmpty"+window.selectIndex]);
+  					if (window["selectIsEmpty"+window.selectIndex]) {
+  						  $(this).siblings('.btn-group').css('border', 'solid 2px red');
+  				          $("#pbf_error_message").show();
+  				          $(this).siblings('.btn-group .button').focus();
+  				          return false;
+  				        }
+  			    }
+ 				
+  			}
+  			if(ind === total_validation_items - 1) {
+  				return submitForm=true;
+  		    }  			
+  		    
+  		});    		
     	  
-    	  $.ajax({
-    	   url:servletURL,
-    	   method:"GET",
-    	   data:form_data,
-           success: function(msg, status, xhr){
-           	var json = msg;
-           	var ct = xhr.getResponseHeader("content-type") || "";
-               if (ct.indexOf('html') > -1) {
-                 json = jQuery.parseJSON(msg);
-               }
-       	    // alert("msg : " + msg);
-            $('#pbf_success_message').show();
-    	    $(".photo-band-form")[0].reset(); // To reset form fields
-/*
-    	    $('#photo-band-form-multiselect option:selected').each(function(){
-    	     $(this).prop('selected', false);
-    	    });
-    	    $('#photo-band-form-multiselect').multiselect('refresh');
-    	    $('#photo-band-form-select option:selected').prop('selected', false);
-    	    $('#photo-band-form-select').multiselect('refresh');
-    	    $('#photo-band-form-textfield').val("");
-    	    // document.getElementById("photo-band-form").reset();
-*/    	    
-    	   }
-    	  });
+/*		$('.photo-band-form').find('.photo-band-form-multiselect, .photo-band-form-select').each(function(ind){
+			// alert("found select field in form");
+    	  // event.preventDefault();
+			$(this).siblings('.btn-group').css('border', 'none');
+		    if($(this).prop('required')){
+		     	window.selectIndex = ind;
+		     	alert(window.selectIndex);
+		    	window["selectIsEmpty"+window.selectIndex] = true;
+		    	
+		    	// selectIsEmpty['select'+i] = true;
+				// alert("required field");
+		    	var form_data = $('.photo-band-form').serializeArray();
+		    	// alert(form_data[2].name);
+		    	var $select_name = $(this).attr('name');
+		    	// alert($select_name)
+				$.each(form_data, function(i, field){
+					if(field.name == $select_name){
+						window["selectIsEmpty"+window.selectIndex] = false;
+					}
+				});
+				alert(window["selectIsEmpty"+window.selectIndex]);
+				if (window["selectIsEmpty"+window.selectIndex]) {
+					  $(this).siblings('.btn-group').css('border', 'solid 2px red');
+			          $("#pbf_error_message").show();
+			          $(this).siblings('.btn-group .button').focus();
+			          return false;
+			        }
+		    }
+		    return submitForm=true;
+		});    		
+*/	    
+    	  if(submitForm){
+        	  var form_data = $('.photo-band-form').serialize();
+        	  // alert(form_data);
+              var current_page_path = $('#current_page_path').val() ; 
+
+              var servletURL = current_page_path + '.photoBandFormServlet.html';
+	    	  $.ajax({
+	    	   url:servletURL,
+	    	   method:"GET",
+	    	   data:form_data,
+	           success: function(msg, status, xhr){
+	           	var json = msg;
+	           	var ct = xhr.getResponseHeader("content-type") || "";
+	               if (ct.indexOf('html') > -1) {
+	                 json = jQuery.parseJSON(msg);
+	               }
+	       	    // alert("msg : " + msg);
+	            $('#pbf_success_message').show();
+	    	    $(".photo-band-form")[0].reset(); // To reset form fields
+	
+	    	    $('.photo-band-form-multiselect option:selected').each(function(){
+	    	     $(this).prop('selected', false);
+	    	    });
+	    	    $('.photo-band-form-multiselect').multiselect('refresh');
+	    	    $('.photo-band-form-select option:selected').prop('selected', false);
+	    	    $('.photo-band-form-select').multiselect('refresh');
+	    	    $('.photo-band-form-textfield').val("");
+	    	    // document.getElementById("photo-band-form").reset();
+	    	    
+	    	   }
+	    	  });
+    	  }
+    	  
     	 });
 });
