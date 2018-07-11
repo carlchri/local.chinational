ce6540299984f38ea8347a771ddd7e73c024e42f
ndevnani <ndevnani@prokarma.com>
2018-07-11 16:12:56 -0600

    initial audio player

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/audio-popup.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/audio-popup.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#audio-modal').on('hidden.bs.modal', function() {
-    	console.log("Hello Simon");
+    	//console.log("Hello Simon");
         var ply = document.getElementById('audio-player');
 
         var oldSrc = ply.src; // just to remember the old source
@@ -9,6 +9,22 @@ $(document).ready(function() {
     });
 
     $('#audio-modal').on('show.bs.modal', function() {
-    	console.log("Hello Simon Modal Show");
-});
+    	//console.log("Hello Simon Modal Show");
     });
+
+    // this is for new audio player
+    // it works with multiple instances of player
+    $('.chi-audio-player').mediaelementplayer({
+            alwaysShowControls: true,
+            features: ['playpause','progress','current','duration', 'volume'],
+            timeAndDurationSeparator: ' / ',
+            audioVolume: 'horizontal',
+            audioWidth: 650,
+            audioHeight: 45,
+            iPadUseNativeControls: true,
+            iPhoneUseNativeControls: true,
+            AndroidUseNativeControls: true
+          });
+    $(".mejs__time").after("<div class='mejs-download-control'><a href='#'>&nbsp;</a></div>");
+
+});
