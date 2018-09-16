d62c5c63582f21d1e0d0899a97a716f8d527b434
sibrown <sibrown@sbrown-pk.local>
2018-09-16 12:35:35 -0600

    Set table functions from table dialog - updated from pulled info

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/table.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/table.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     // console.log("Table JS ready!");
-	var table = $('table');
+    var table = $('table');
     var tableCount = 0;
     table.each(function(){
         tableCount++;
@@ -10,55 +10,55 @@ $(document).ready(function(){
         var caption = $(this).find('caption');
         var tabw = $(this).attr('width') || '216';
         var tabh = $(this).attr('height') || 'auto';
-        var tabDWidth = $(this).find('td:first').attr('width') ;
-        var tabDWHeaderCellWidth = $(this).find('th:first').attr('width');
+        var tabDWidth = $(this).find('td:first').attr('width' ) || 216 ;
+        var tabDWHeaderCellWidth = $(this).find('th:first').attr('width') || 216;
         var tabDWHeaderWidth = 0;
 
-        console.log("TabDWidth : "+tabDWidth);
-        console.log("tabDWHeaderCellWidth : "+tabDWHeaderCellWidth);
+        // console.log("Table width : "+tabw);
+        // console.log("TabDWidth : "+tabDWidth);
+        // console.log("tabDWHeaderCellWidth : "+tabDWHeaderCellWidth);
 
-        $(this).find('tr:first td').each(function(){
-            $(this).width(tabDWidth);
-			tdCount++;
-        });
         $(this).find('tr:first th').each(function(){
-			thCount++;
+            thCount++;
         });
+        // console.log("TH Count :"+thCount);
 
-        tableWidth = tabw*tdCount;
+        $(this).find('tr:last td').each(function(){
+            $(this).width(tabDWidth);
+            tdCount++;
+        });
+        // console.log("Table Data Width : "+tabDWidth);
+        // console.log("TD Count : "+tdCount);
+
+        tableWidth = tabw;
         tabDWHeaderWidth = tabDWHeaderCellWidth*thCount;
 
-        if (tableWidth)
-        	console.log('tableWidth : '+tableWidth+' tabw : '+tabw+' tdCount : '+tdCount );
+        // if (tableWidth)
+        //     console.log('tableWidth : '+tableWidth+', tabDWidth : '+tabDWidth+', tdCount : '+tdCount );
 
-        if (tabDWHeaderWidth)
-        	console.log('tableWidth : '+tabDWHeaderWidth+' tabw : '+tabw+' thCount : '+thCount );
+        // if (tabDWHeaderWidth)
+        //     console.log('tableWidth : '+tabDWHeaderWidth+' tabw : '+tabw+' thCount : '+thCount );
 
         $(this).removeClass('wHeader');
         $(this).addClass('noHeader');
         $(this).width(tableWidth).height(tabh).css('overflow-y','auto').css('overflow-x','hidden').css('display','block');
-        $(this).wrap('<div class="chi-table"><div class="scroller-container" style="min-width: '+tableWidth+'px"></div></div>');
+        $(this).wrap('<div class="chi-table"><div class="scroller-container" style="width: '+tableWidth+'px"></div></div>');
         var tHeader = $(this).find('tr th');
-        if (tHeader.length) {
-            var newTHeader = tHeader.parent('tr').html();
+        if (tHeader.length ) {
+            var newTHeaderHTML = tHeader.parent('tr').html();
             var newCaption = caption;
             tHeader.remove();
             caption.remove();
+            $(this).removeClass('noHeader');
+            $(this).addClass('wHeader');
             $(this).width(tabDWHeaderWidth);
-            $(this).parent('.scroller-container').prepend('<table class="headerTable'+tableCount+'" style="width: '+tabDWHeaderWidth+'px;">'+newTHeader+'</table>');
-
-
-
+            $(this).parent('.scroller-container').prepend('<table class="headerTable'+tableCount+'" style="width: '+tabDWHeaderWidth+'px;">'+newTHeaderHTML+'</table>');
 
             if (newCaption)
-				$(this).parent('.scroller-container').prepend(newCaption);
+                $(this).parent('.scroller-container').prepend(newCaption);
 
-            $(this).removeClass('noHeader');
-        	$(this).addClass('wHeader');
-
-            var cssTemplate = '<style>table.headerTable th{ background:red}</style>';
-            $('head').append(cssTemplate);
-			$(this).find('td').width(tabDWHeaderCellWidth);
+            $(this).find('th').width(tabDWHeaderCellWidth);
+            $(this).find('td').width(tabDWHeaderCellWidth);
             $('<div />', { html: '&shy;<style> table.headerTable'+tableCount+' th {width :'+tabDWHeaderCellWidth+'px;} table.headerTable'+tableCount+' td {width :'+tabDWHeaderCellWidth+'px;}</style>' }).appendTo("body");
         }
     });
