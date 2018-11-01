47bb37f78f04bc9dcec2691ddbf4e3de07d57620
German Pelaez <gpelaez@prokarma.com>
2018-11-01 13:11:22 -0600

    Second batch of files for Location Component

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/location.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/location.js
@@ -1,11 +1,11 @@
 
 $(function(){
-	$("#image").load(function(){
+	//$("#image").load(function(){
 		
 		// Getting the natural size of the image
-		var image = new Image();
-		image.src = $("img", this).attr("src");
-		console.log('width: ' + image.naturalWidth + ' and height: ' + image.naturalHeight);
+	//	var image = new Image();
+	//	image.src = $("img", this).attr("src");
+	//	console.log('width: ' + image.naturalWidth + ' and height: ' + image.naturalHeight);
 
-	});
+	//});
 });
