452f7823737a0f566d13dd3f003f1ef050002d04
German Pelaez <gpelaez@prokarma.com>
2018-11-05 09:44:50 -0700

    Fixing MAM-160 as per Zeplin directions to keep 16:9 ratio

--- ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/location.js
+++ ui.apps/src/main/content/jcr_root/apps/chinational/clientlibs/clientlib-base/js/location.js
@@ -1,11 +1,38 @@
 
-$(function(){
-	//$("#image").load(function(){
-		
-		// Getting the natural size of the image
-	//	var image = new Image();
-	//	image.src = $("img", this).attr("src");
-	//	console.log('width: ' + image.naturalWidth + ' and height: ' + image.naturalHeight);
+function adjustHeightWidth() {
+	
+	$('#myLocationComponent #image img').each(function() {
+						
+		var tds = $(this);
+		var image = new Image();
+		image.src = tds.attr("src");
 
-	//});
+		if (image.naturalWidth > 703 && image.naturalHeight > 395) {
+			$(this).removeClass();
+			$(this).addClass("image-responsive-3");
+
+		} else {
+			if (image.naturalWidth > 703 && image.naturalHeight <= 395) {
+				$(this).removeClass();
+				$(this).addClass("image-responsive");
+
+			} else {
+				if (image.naturalWidth <= 703 && image.naturalHeight > 395) {
+					$(this).removeClass();
+					$(this).addClass("image-responsive-2");
+
+				} else {
+					if (image.naturalWidth <= 703 && image.naturalHeight <= 395) {
+						$(this).removeClass();
+						$(this).addClass("image-responsive-4");
+					
+					}
+				}
+			}
+		}
+	});
+}
+
+$(window).on("load", function() {
+	adjustHeightWidth();
 });
